5e10d1e2f923794ef284db073dbfcf6293dfc08e
Jerry Xu <mxujry@gmail.com>
2020-06-02 14:41:25 +0800

    added 'comp-lzo no' remova

--- add_auth.py
+++ add_auth.py
@@ -27,8 +27,8 @@ def read_or_create_login():
     except IndexError:
         print('file is corrupt, re-creating login.conf')
         create_login()
-        
-        
+
+
 def create_login():
     with open(dir + '/login.conf', 'w+') as f:
         print('Please enter credentails...')
@@ -36,24 +36,29 @@ def create_login():
         f.write(username + '\n')
         password = input('Password: ')
         f.write(password)
-        
-        
+
+
 def edit_file(path):
     for line in fileinput.input(path, inplace=True):
+        #add autehntication config
         print(re.sub('^auth-user-pass.*', 'auth-user-pass login.conf', line, 1), end='')
-        
+
+    for line in fileinput.input(path, inplace=True):
+        #delete 'comp-lzo no' to prevent deprecation warning
+        print(re.sub('^comp-lzo no.*', '', line, 1), end='')
+
 def process_files():
     with os.scandir(dir + '/') as it:
         for entry in it:
             if entry.name.endswith('ovpn') and entry.is_file():
                 edit_file(dir + '/' + entry.name)
-    
+
 def main():
     if '-f' in opts:
         create_login()
     else:
         read_or_create_login()
-        
+
     process_files()
 
 
